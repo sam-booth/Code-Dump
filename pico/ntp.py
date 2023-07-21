658b47663094e1e78d41e26f9c2da9046bfa5acc
Sam Booth <sam@booth.org.uk>
2023-07-21 21:44:02 +0100

    Update ntp.py
    Fix variable name and make output more human readable.

--- pico/ntp.py
+++ pico/ntp.py
@@ -12,6 +12,6 @@ print (rtc.datetime()) # get date and time
 year = utime.localtime()[0]
 month = utime.localtime()[1]
 day = utime.localtime()[2]
-hourMin = (str(utime.localtime()[3]) + f'{utime.localtime()[4]:02}')
+hourMin = (str(utime.localtime()[3]) + str(":") + f'{utime.localtime()[4]:02}')
 
-result = (str(year) + "/" + f'{month:02}' + "/" + str(day) + "/" + str(number))
+result = (str(year) + "/" + f'{month:02}' + "/" + str(day) + " Time:" + str(hourMin))
